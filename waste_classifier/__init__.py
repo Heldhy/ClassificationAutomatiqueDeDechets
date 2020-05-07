903d5c9db299829a3feaee38f6181144a374eb2f
Danae Marmai <danae.marmai@epita.fr>
2020-05-07 17:16:28 +0200

    dupplicate import

--- waste_classifier/__init__.py
+++ waste_classifier/__init__.py
@@ -17,7 +17,6 @@ optimizer_type = "rmsprop"
 from .training import *
 from .data_augmentation import *
 from .models import *
-from .models import *
 from .post_processing import *
 from .pre_processing import *
 from .use_model import *
