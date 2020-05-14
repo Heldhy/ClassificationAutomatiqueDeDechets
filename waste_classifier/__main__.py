2657b063666f7d8bce960cbbf18a19dbd70cfd27
Danae Marmai <danae.marmai@epita.fr>
2020-05-14 17:54:41 +0200

    changing function name

--- waste_classifier/__main__.py
+++ waste_classifier/__main__.py
@@ -1,10 +1,10 @@
-from waste_classifier import pre_processing, create_generator, save_generated_batch, training, \
+from waste_classifier import pre_processing, create_new_generator, save_generated_batch, training, \
     evaluate_post_processed_prediction
 
 
 def main():
     x_train, y_train, x_test, y_test = pre_processing()
-    train_generator, val_generator = create_generator(x_train, y_train)
+    train_generator, val_generator = create_new_generator(x_train, y_train)
     save_generated_batch(train_generator, 2)
     model = training(train_generator, val_generator, x_test, y_test, "vgg16", True)
     evaluate_post_processed_prediction(model, x_test, y_test)
